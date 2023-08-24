2de82dea7cedf0240f33498212a0f8c2af65f2f1
mval23 <111825282+mval23@users.noreply.github.com>
2023-08-24 18:19:30 -0500

    changes

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -3,9 +3,9 @@ package taller3.televisores;
 public class TV {
     private Marca marca;
     private int canal = 1;
-    private int precio = 1;
+    private int precio = 500;
     private boolean estado;
-    private int volumen = 500;
+    private int volumen = 1;
     private Control control;
     private static int numTV;
     public TV(Marca marca, boolean estado) {
